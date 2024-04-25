efc1d562258d30ea3c6f04985f40417b73439e96
Swarnavo Mukherjee <swarnavomukherjee03@gmail.com>
2024-04-25 14:28:26 +0530

    complete-test

--- mine.py
+++ mine.py
@@ -1,11 +1,20 @@
 import hashlib 
 
-LIMIT = 90000000000000
+LIMIT = 90000000000000000000000
 
-zeros = 4
+zeros = 6
 
-def mine(block_number,transactions,prev_hash):
-    for number in limit(LIMIT):
+def mine_coins(block_number,transactions,prev_hash):
+    for number in range(LIMIT):
         base_text = str(block_number) + transactions + prev_hash + str(number)
-        
+        hash_trial = hashlib.sha256(base_text.encode()).hexdigest()
+        if hash_trial.startswith('0' * zeros):
+            print(f"Hash found successfully with number: {number} ")
+            return hash_trial 
+    return -1 
+
+block_number = 24 
+transactions = "7638ucheufm97"
+previous_hash = "3728afjhfdj88"
    
+mine_coins(block_number,transactions,previous_hash)
